cdaed1c61c367052f1a93a7b989bcb076f6e2de8
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-17 19:27:43 +0300

    feat(main): initialize the db at the start of the app's lifespan

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,16 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
+from db import init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+
+    yield
+
 
 app = FastAPI()
 
