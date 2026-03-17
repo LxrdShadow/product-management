f3c7f5cc56f631d5fa5b856805925f7ad02fca88
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-17 20:42:36 +0300

    feat(app): separate app from main

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,4 @@
-from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
-
-from db import init_db
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_db()
-
-    yield
-
-
-app = FastAPI()
+from app.app import app
 
 
 @app.get("/")
