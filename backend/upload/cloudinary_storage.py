6ebe89475425a38d1c2cbd77a66ed30777010967
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-22 17:20:06 +0300

    fix(storage): fix cloudinary storage's save method to return the right id

--- backend/upload/cloudinary_storage.py
+++ backend/upload/cloudinary_storage.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Tuple
 
 import cloudinary
@@ -18,15 +19,16 @@ cloudinary.config(
 
 class CloudinaryStorage(Storage):
     def __init__(self, cloud_name: str) -> None:
-        self.base_url = (
-            f"https://res.cloudinary.com/{cloudinary.cloud_name}/image/upload"
-        )
+        self.base_url = f"https://res.cloudinary.com/{cloud_name}/image/upload"
 
     def save(self, path: str, content: bytes) -> str:
+        public_id = str(Path(path).with_suffix(""))
         result = cloudinary.uploader.upload(
-            content, public_id=path, folder="product-management"
+            content, public_id=public_id, folder="product-management"
         )
-        return result.get("public_id")
+        return (
+            f"v{result.get('version')}/{result.get('public_id')}.{result.get('format')}"
+        )
 
     def fetch(self, id: str) -> Tuple[bytes, str]:
         url = f"{self.base_url}/{id}"
